7934228078f46dff88c651ee9ec9f5104766ffdb
Ivan <ivanmw97@gmail.com>
2025-04-01 02:05:38 +0100

    GG-013 footer hover removed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -25,7 +25,7 @@
 
     <!-- Footer -->
     <div class="p-4 border-t border-gray-800">
-      <div class="flex items-center gap-3 px-2 py-2 rounded-lg hover:bg-gray-800 transition-colors">
+      <div class="flex items-center gap-3 px-2 py-2 rounded-lg">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
           <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
           <path fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
@@ -39,7 +39,7 @@
   </div>
 </template>
   
-  <script setup lang="ts">
+<script setup lang="ts">
   import SidebarItem from '../components/SidebarItem.vue';
-  </script>
+</script>
   
